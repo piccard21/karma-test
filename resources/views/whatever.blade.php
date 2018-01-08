2f7b976f7f917d1cd91c40d75c2b6fc39c5b2469
Andreas Stephan <andreas.stephan@internetx.com>
2018-01-08 12:07:31 +0100

    simple component

--- resources/views/whatever.blade.php
+++ resources/views/whatever.blade.php
@@ -94,6 +94,36 @@
 		<ajax></ajax>
 
 		<hr>
+
+		{{--Forms--}}
+		<h3>Forms</h3>
+		<a href="https://github.com/laracasts/Vue-Forms">see on github</a>
+
+		<br>
+
+		<form method="POST" action="/projects" @submit.prevent="onSubmit" @keydown="form.errors.clear($event.target.name)">
+			<div class="control">
+				<label for="name" class="label">Project Name:</label>
+
+				<input type="text" id="name" name="name" class="input" v-model="form.name">
+
+				<span class="help is-danger" v-if="form.errors.has('name')" v-text="form.errors.get('name')"></span>
+			</div>
+
+			<div class="control">
+				<label for="description" class="label">Project Description:</label>
+
+				<input type="text" id="description" name="description" class="input" v-model="form.description">
+
+				<span class="help is-danger" v-if="form.errors.has('description')" v-text="form.errors.get('description')"></span>
+			</div>
+
+			<div class="control">
+				<button class="button is-primary" :disabled="form.errors.any()">Create</button>
+			</div>
+		</form>
+
+		<hr>
 		<hr>
 		<hr>
 		<hr>
